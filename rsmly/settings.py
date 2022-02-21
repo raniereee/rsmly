85f1f00e8b4757248674d03557ce3a0e8c456c0d
Raniere dos Santos Machado <raniere.ee@gmail.com>
2022-02-21 09:35:23 -0300

    adjusted time zone

--- rsmly/settings.py
+++ rsmly/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Custom apps
+    'urlshortener',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
